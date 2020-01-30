783496a08c5fe046c0ae6a2f7dced7bcb3bb1d55
Kuznetsova Natalya <natalya.kuznetsova@syngenta.com>
2020-01-30 10:42:01 +0300

    Type Basics

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,73 @@ function showHello(divName: string, name: string) {
   const elt = document.getElementById(divName);
   elt.innerText = `Hello from ${name}`;
 }
+
+// -----------TS-in-Depth--------------- //
+
+enum Category {JavaScript, CSS, HTML, TypeScript, Angular};
+
+function getAllBooks(): object[] {
+  const books: object[] = [
+    { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JavaScript },
+    { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JavaScript },
+    { title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
+    { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JavaScript }
+    ]
+
+  return books;  
+}
+
+function logFirstAvailable(books: readonly object[]): void {
+  const num: number = books.length;
+  let title: string = '';
+
+  for (const book of books) {
+    if (book['available']) {
+      title = book['title'];
+      break;
+    }
+  }
+  console.log(num);
+  console.log(title);
+}
+
+function getBookTitlesByCategory(category: Category): Array<string> {
+  const books = getAllBooks();
+  const titles: string[] = [];
+
+  for (const book of books) {
+    if ((<any>book).category === category) {
+      titles.push(book['title']);
+    }
+  }
+  return titles;
+}
+
+function logBookTitles(titles: string[]): void {
+  titles.forEach((title: string) => console.log(title));
+}
+
+function getBookAuthorByIndex(index: number): [string, string] {
+  const books = getAllBooks();
+  const { title, author } = books[index] as any;
+  return [title, author];
+}
+
+function calcTotalPages(): bigint {
+  const data = <const>[
+    { lib: 'libName1', books: 1_000_000_000, avgPagesPerBook: 250 },
+    { lib: 'libName2', books: 5_000_000_000, avgPagesPerBook: 300 },
+    { lib: 'libName3', books: 3_000_000_000, avgPagesPerBook: 280 },
+  ];
+
+  const pages: bigint = data.reduce((acc: bigint, obj: any) => {
+    return  acc + BigInt(obj.books) * BigInt(obj.avgPagesPerBook);
+  }, BigInt(0));
+
+  return pages;
+}
+
+logFirstAvailable(getAllBooks());
+logBookTitles(getBookTitlesByCategory(Category.JavaScript));
+console.log(getBookAuthorByIndex(2));
+console.log(calcTotalPages())
